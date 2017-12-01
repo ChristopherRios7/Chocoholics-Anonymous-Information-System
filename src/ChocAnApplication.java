d901b945bfaee16a6eafea6f3d3802b3b17f07f6
Jeremy Luna <jeremylunaalpha@gmail.com>
2017-12-01 16:07:19 -0600

    All classes exist. I hope these packages are logical.

---
+++ src/ChocAnApplication.java
@@ -0,0 +1,9 @@
+
+public class ChocAnApplication {
+
+    public static void main(String[] args) {
+        
+
+    }
+
+}
---
+++ src/maintainers/ProviderMaintainer.java
@@ -0,0 +1,5 @@
+package maintainers;
+
+public class ProviderMaintainer {
+
+}
